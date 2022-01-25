c4087ed284b5eb351caae4a787e0a863f88566f9
Harshita2905 <65132209+Harshita2905@users.noreply.github.com>
2022-01-25 17:49:37 +0530

    Add files via upload

---
+++ W3_day2/Practice/Challenge_Assignment/Stack.java
@@ -0,0 +1,44 @@
+package Challenge_Assignment;
+
+import java.io.*;
+import java.util.*;
+
+public class Stack {
+    private static void getMaxElementFromStack() {
+        Stack <Integer> stack = new Stack<>();
+        Stack <Integer> onlyMaxs = new Stack<>();
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = Integer.parseInt(sc.nextLine());
+        int temp = 0;
+
+        while (sc.hasNext()) {
+            String type[] = sc.nextLine().split(" ");
+            switch (type[0]) {
+                case "1":
+                    temp = Integer.parseInt(type[1]);
+                    stack.push(temp);
+                    if (onlyMaxs.isEmpty() || onlyMaxs.peek() <= temp)
+                        onlyMaxs.push(temp);
+                    break;
+                case "2":
+                    temp = stack.pop();
+                    if (temp == onlyMaxs.peek())
+                        onlyMaxs.pop();
+                    break;
+                case "3":
+                    System.out.println(onlyMaxs.peek());
+            }
+            N--;
+        }
+
+        while (N-- > 0)
+            System.out.println(onlyMaxs.peek());
+
+    }
+
+    public static void main(String[] args) {
+        getMaxElementFromStack();
+    }
+}
