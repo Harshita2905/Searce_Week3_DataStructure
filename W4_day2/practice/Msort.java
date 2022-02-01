178d27fa51235f88e564ebc930326696857d7433
Harshita2905 <65132209+Harshita2905@users.noreply.github.com>
2022-02-01 14:30:28 +0530

    Add files via upload

---
+++ W4_day2/practice/Msort.java
@@ -0,0 +1,75 @@
+package sort;
+import jdk.incubator.foreign.SequenceLayout;
+
+class Msort {
+    void merge(int arr[], int l, int m, int r) {
+
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    void print(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + "\t");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 12, 11, 13, 5, 6, 7 };
+        Msort ob = new Msort();
+        System.out.println("Given Array");
+        ob.print(arr);
+
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array");
+        ob.print(arr);
+    }
+
+}
